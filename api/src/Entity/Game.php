a2556f76e1af2375d13a32ee68618877347030fc
kevin.luebrecht <kevin.luebrecht@gutscheinbuch.de>
2023-07-04 13:49:35 +0200

    added NotBlank Validations

--- api/src/Entity/Game.php
+++ api/src/Entity/Game.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: GameRepository::class)]
 #[ApiResource(
@@ -21,6 +22,7 @@ class Game
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
